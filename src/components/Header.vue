1d08ebc7a49682e5f21684369a1f7ebae9f161f2
Victor Moreira <victormoreira176@hotmail.com>
2020-03-30 16:17:42 -0300

    starting header and adding external font

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,15 @@
 <template>
   <div id="header-out">
     <div class="header-elements">
-      <div class="header-logo">Espatodea</div>
+      <div class="header-logo">
+        <img src="./../assets/espatodea.png" style="width: 110px;height:110px;">
+      </div>
       <div class="header-menu">
         <ul>
-          <router-link to="/post/form" class="no-decoration"><li>Post Form</li></router-link>
-          <router-link to="/home" class="no-decoration"><li>Test Component</li></router-link>
+          <router-link to="/post/form" class="no-decoration"><li>Meio Ambiente</li></router-link>
+          <router-link to="/home" class="no-decoration"><li>Entretenimento</li></router-link>
+          <router-link to="/post/form" class="no-decoration"><li>Alimentação</li></router-link>
+          <router-link to="/post/form" class="no-decoration"><li>Entenda mais</li></router-link>
         </ul>
       </div>
     </div>
@@ -27,20 +31,19 @@ ul {
 li {
   margin-right: 10px;
   padding: 5px;
-  border: 1px solid white;
 }
 .no-decoration {
   text-decoration: none;
   color: white;
 }
 #header-out {
-        width: 100%;
-        height: 80px;
-        background-color: darksalmon;
-        display: flex;
-        align-items: center;
-        margin-bottom: 40px;
-    }
+  width: 100%;
+  height: 80px;
+  background-color: #F3B69B;
+  display: flex;
+  align-items: center;
+  margin-bottom: 40px;
+}
 .header-elements {
   width: 100%;
   display: flex;
@@ -49,11 +52,16 @@ li {
   padding: 10px;
 }
 .header-logo {
-  margin-left: 50px;
+  margin-left: 10%;
 
   color: white;
 }
 .header-menu {
-  margin-right: 200px;
+  margin-right: 10%;
+  font-family: "Quicksand Light";
+}
+@font-face {
+  font-family: "Quicksand Light";
+  src: url("./../assets/fonts/Quicksand_Light.otf") format("otf");
 }
 </style>
