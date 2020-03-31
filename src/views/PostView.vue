f72796d13e4454572e7b91215269beed5aa6b56f
Victor Moreira <victormoreira176@hotmail.com>
2020-03-31 00:39:23 -0300

    converting \n to enter

---
+++ src/views/PostView.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="outter-post-view">
+        <button @click="getPostById(24)">Get the post</button>
+        <div v-html="(post.data != undefined) ? post.data.data.post_content : ''"></div>
+    </div>
+</template>
+
+<script>
+import { RepositoryFactory } from './../api-calls/RepositoryFactory';
+// import Post from './../models/Post';
+const postCaller = RepositoryFactory.get('posts');
+
+export default {
+    data () {
+        return {
+            post: []
+        }
+    },
+    methods: {
+        async getPostById(id) {
+            this.post = await postCaller.getById(id);
+            this.post.data.data.post_content = this.post.data.data.post_content.replace(/\n/g, "<br />");
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
