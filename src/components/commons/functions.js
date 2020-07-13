784e7ae603e797d0e5cb2c1769590aa2ba538789
Victor Moreira <victormoreira176@hotmail.com>
2020-07-12 23:02:08 -0300

    fixing date handling and simplifying some functons at postForm

--- src/components/commons/functions.js
+++ src/components/commons/functions.js
@@ -1,6 +1,6 @@
 export function americanDate(date) {
     let date_converted = new Date(date);
-    let day = (date_converted.getUTCDate() < 10) ? `0${date_converted.getUTCDate()}` : date_converted.getUTCDate();
+    let day = (date_converted.getDate() < 10) ? `0${date_converted.getDate()}` : date_converted.getDate();
     let month = (date_converted.getMonth()+1 < 10) ? `0${date_converted.getMonth()+1}` : date_converted.getMonth()+1;
     let year = date_converted.getFullYear();
     return `${year}-${month}-${day}`;
