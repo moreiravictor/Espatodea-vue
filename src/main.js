8f36c31c94421ff4a8750d54cbe3d82f98847708
Victor Moreira <victormoreira176@hotmail.com>
2020-07-13 23:40:04 -0300

    adding and configuring quill editor to postForm

--- src/main.js
+++ src/main.js
@@ -7,8 +7,26 @@ import { faFontAwesome } from '@fortawesome/free-brands-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import VueSimpleAlert from "vue-simple-alert";
+import VueQuillEditor from 'vue-quill-editor'
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css' 
 
 Vue.use(VueSimpleAlert);
+Vue.use(VueQuillEditor,  { 
+  placeholder: 'Escreva aqui...' ,
+  modules: {
+    toolbar: {
+      container: [
+        ['bold', 'italic', 'underline', 'strike'], 
+        ['link', 'image'],  
+        [{ 'list': 'ordered'}, { 'list': 'bullet' }],  
+        [{ 'align': [] }], 
+        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+        [{ 'size': ['small', false, 'large', 'huge'] }]
+      ]
+    }
+  }
+});
 library.add(fab)
 library.add(fas)
 library.add(faFontAwesome)
