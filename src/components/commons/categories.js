a511af417c1cf288dbc643b63c2175a97957273c
Victor Moreira <victormoreira176@hotmail.com>
2020-07-17 00:01:19 -0300

    adding  related posts component/adding pagination do api call

--- src/components/commons/categories.js
+++ src/components/commons/categories.js
@@ -7,5 +7,30 @@ export const categories = {
     5:	"Projetos",
     6:	"Dicas práticas",
     7:	"Dicas de alimentação",
-    8:	"Receitas"
+    8:  "Receitas",
+    9:  "Plantar"
+}
+
+export function getMostRelatedCategory(post_categories) {
+    let relevance = {
+        "Comece aqui":          9,
+        "Alimentação":          8,
+        "Plantar":              7,
+        "Entenda mais":         6,
+        "Projetos":             5,
+        "Na prática":           4,
+        "Receitas":             3,
+        "Dicas de alimentação": 2,
+        "Dicas práticas":       1,
+        "Blog":                 0
+    }
+    let most_related = {relevance: 0, id: 9};
+    post_categories.forEach(category => {
+        let category_relevance = relevance[category.category_description];
+        if (category_relevance > most_related.relevance) {
+            most_related.relevance = category_relevance;
+            most_related.id = category.category_id;
+        }
+    });
+    return most_related.id;
 }
